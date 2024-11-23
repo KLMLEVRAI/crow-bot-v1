f330e019047aff6a1879418ebb3eb90fef54cc9f
KLM <122469699+KLMLEVRAI@users.noreply.github.com>
2024-11-23 16:28:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})

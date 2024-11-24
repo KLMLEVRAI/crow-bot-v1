daae92edf420ee73e0805ca71dbe43b2c8388a2c
KLM <122469699+KLMLEVRAI@users.noreply.github.com>
2024-11-24 09:24:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 4000;
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
+// Route par défaut
+app.get('/', (req, res) => {
+  res.send('Bot Discord is running!');
+});
+
+// Lancer le serveur pour satisfaire Render
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Express server is running on port ${port}`);
+});
